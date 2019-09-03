a0697d61f277b08eb8c67df7221c431c3b1abb1c
Borut Paškulin <borut.paskulin@rtvslo.si>
2019-09-03 15:02:31 +0200

    add test

--- input/Validator.php
+++ input/Validator.php
@@ -2,138 +2,149 @@
 
 class Validator
 {
-    public function apply($obj, $rules)
+    public static function apply($obj, $rules)
     {
         $error = [];
         foreach ($rules as $prop_name=>$rule_set){
-            
+
             $property_is_set = isset($obj[$prop_name]);
 
             foreach ($rule_set as $rule){
-            
+
                 if (!$property_is_set){
                     if (in_array('required', $rule_set)){
                         $error[] = "Missing '$prop_name'";
                     }
                     break; //goto next property
                 }
-            
+
                 $args = explode(':', $rule);
                 $cmd = array_shift($args);
-            
+
                 switch ($cmd){
                     case 'is_num':
-                        if (!is_numeric($obj[$prop_name])){
-                            $error[] = "Not a number '$prop_name' (".$obj[$prop_name].")";
+                        if (!self::is_num($obj[$prop_name])){
+                            $error[] = "Not numeric '$prop_name' (".$obj[$prop_name].")";
                         }
-                        continue;
-                    
+                        break;
+
                     case 'is_id_num':
-                        if (!$this->is_id_num($obj[$prop_name])){
+                        if (!self::is_id_num($obj[$prop_name])){
                             $error[] = "Invalid ID '$prop_name' (".$obj[$prop_name].")";
                         }
-                        continue;
-                    
+                        break;
+
                     case 'is_id_num_array':
-                        if (!$this->is_id_num_array($obj[$prop_name])){
+                        if (!self::is_id_num_array($obj[$prop_name])){
                             $error[] = "Invalid array of IDs '$prop_name'";
                         }
-                        continue;
-                    
+                        break;
+
                     case 'is_time':
-                        if (!$this->is_time($obj[$prop_name])){
+                        if (!self::is_time($obj[$prop_name])){
                             $error[] = "Invalid time '$prop_name' (".$obj[$prop_name].")";
                         }
-                        continue;
+                        break;
 
-		    case 'is_date':
-                        if (!$this->is_date($obj[$prop_name])){
+                    case 'is_date':
+                        if (!self::is_date($obj[$prop_name])){
                             $error[] = "Invalid date '$prop_name' (".$obj[$prop_name].")";
                         }
-                        continue;
+                        break;
 
-		    case 'is_timestamp':
-                        if (!$this->is_timestamp($obj[$prop_name])){
+                    case 'is_timestamp':
+                        if (!self::is_timestamp($obj[$prop_name])){
                             $error[] = "Invalid timestamp '$prop_name' (".$obj[$prop_name].")";
                         }
-                        continue;
+                        break;
 
                     case 'is_timestamp_range':
-                        if (!$this->is_timestamp_range($obj[$prop_name])){
+                        if (!self::is_timestamp_range($obj[$prop_name])){
                             $error[] = "Invalid timestamp range '$prop_name'";
                         }
-                        continue;
-                    
-                    
+                        break;
+
                     case 'is_not_empty_string':
-                        if (!$this->is_not_empty_string($obj[$prop_name])){
+                        if (!self::is_not_empty_string($obj[$prop_name])){
                             $error[] = "Empty string '$prop_name'";
                         }
-                        continue;
-                    
-                    
+                        break;
+
                     case 'is_string':
-                        if (!is_string($obj[$prop_name])){
+                        if (!self::is_string($obj[$prop_name])){
                             $error[] = "Not a string '$prop_name'";
                         }
-                        continue;
-                    
-                    
-                    case 'is_boolean':
-                        if (!is_bool($obj[$prop_name])){
+                        break;
+
+                    case 'is_bool':
+                        if (!self::is_bool($obj[$prop_name])){
                             $error[] = "Not boolean '$prop_name'";
                         }
-                        continue;
-
+                        break;
 
                     case 'is_string_alphanumeric':
-                        if (!is_string_alphanumeric($obj[$prop_name])){
+                        if (!self::is_string_alphanumeric($obj[$prop_name])){
                             $error[] = "Not alpha-numeric string '$prop_name'";
                         }
-                        continue;
-
+                        break;
 
                     case 'is_string_numeric':
-                        if (!is_string_numeric($obj[$prop_name])){
+                        if (!self::is_string_numeric($obj[$prop_name])){
                             $error[] = "Not a numeric string '$prop_name'";
                         }
-                        continue;
-
+                        break;
 
                     case 'is_string_alpha':
-                        if (!is_string_alpha($obj[$prop_name])){
+                        if (!self::is_string_alpha($obj[$prop_name])){
                             $error[] = "Not a alpha string '$prop_name'";
                         }
-                        continue;
-
+                        break;
 
                     case 'is_ip_address':
-                        if (!is_ip_address($obj[$prop_name])){
+                        if (!self::is_ip_address($obj[$prop_name])){
                             $error[] = "Not valid IP address '$prop_name'";
                         }
-                        continue;
+                        break;
                 }
             }
         }
         return $error;
     }
-   
-    private function is_id_num($id)
+
+    public static function is_bool($var)
+    {
+        return is_bool($var);
+    }
+
+    public static function is_num($id)
+    {
+        return (is_numeric($id)) ? true : false;
+    }
+
+    public static function is_id_num($id)
     {
         return (is_integer($id) && $id > 0) ? true : false;
     }
 
-    private function is_id_num_array($arrid)
+    public static function is_id_num_array($arrid)
     {
         if (!is_array($arrid))
             return false;
-        
+
         foreach ($arrid as $id){
-            if (!$this->is_id_num($id)){
+            if (!self::is_id_num($id)){
                 return false;
             }
         }
-        
+
+        return true;
+    }
+
+    public static function is_timestamp($ts)
+    {
+        if (preg_match('/^\d\d\d\d-\d{1,2}-\d{1,2}[T ]\d{1,2}:\d{1,2}:[\d\.+]+$/', $ts) !== 1)
+            return false;
+
         return true;
     }
 
@@ -145,7 +156,7 @@ class Validator
      * ['', '']
      * [null, null]
      */
-    private function is_timestamp_range($range)
+    public static function is_timestamp_range($range)
     {
         if (!is_array($range)) return false;
         if (count($range) != 2) return false;
@@ -153,68 +164,68 @@ class Validator
         if (!(self::is_timestamp($range[1]) || self::is_empty_string($range[1]) || is_null($range[1]))) return false;
         return true;
     }
- 
-    private function is_time($ts)
+
+    public static function is_time($ts)
     {
-        if (preg_match('/^\d{1,2}:\d{1,2}:[\d\.+]+$/', $ts) !== 1)
+        if (preg_match('/^\d{1,2}:\d{1,2}(:[\d\.+]+)*$/', $ts) !== 1)
             return false;
 
         return true;
     }
-    
-    private function is_date($ts)
+
+    public static function is_date($ts)
     {
         if (preg_match('/^\d\d\d\d-\d{1,2}-\d{1,2}$/', $ts) !== 1)
             return false;
 
         return true;
     }
- 
-    private function is_timestamp_range($range)
-    {
-        if (!is_array($range)) return false;
-        if (count($range) != 2) return false;
-        if (!self::is_timestamp($range[0]) && !self::is_empty_string($range[0]) && !is_null($range[0])) return false;
-        if (!self::is_timestamp($range[1]) && !self::is_empty_string($range[1]) && !is_null($range[1])) return false;
-        return true;
-    }
-    
-    private function is_empty_string($str)
+
+    public static function is_empty_string($str)
     {
         if (is_string($str) && empty($str)) return true;
         return false;
     }
-    
-    private function is_not_empty_string($str)
+
+    public static function is_string($str)
+    {
+        return is_string($str);
+    }
+
+    public static function is_not_empty_string($str)
     {
         if (is_string($str) && !empty($str)) return true;
         return false;
     }
-    
-    private function is_ip_address($str)
+
+    public static function is_ip_address($str)
     {
-	if (preg_match("/^\d{1,3}\.\d{1,3}\.\d{1,3}\.d{1,3}$/", $str) == 1) return true;
-	return false;
-    }
-    
-    private function is_string_alphanumeric($str)
-    {
-        if (preg_match("/[^a-zA-Z0-9čšžČŠŽ]+/", $str) == 1) return true;
+        if (preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $str) == 1) return true;
         return false;
     }
-    
-    private function is_string_alpha($str)
+
+    public static function is_string_alphanumeric($str)
     {
-        if (preg_match("/[^a-zA-ZčšžČŠŽ]+/", $str) == 1) return true;
-        return false;
+		if (!is_string($str)) return false;
+        //if (preg_match('/[^a-zA-Z0-9čšžČŠŽ]+/', $str) == 1) return false;
+        if (preg_match('/[^a-zA-Z0-9]+/', $str) == 1) return false;
+        return true;
     }
-    
-    private function is_string_numeric($str)
+
+    public static function is_string_alpha($str)
     {
-        if (preg_match("/[^0-9]+/", $str) == 1) return true;
-        return false;
+		if (!is_string($str)) return false;
+        if (preg_match('/[^a-zA-ZčšžČŠŽ]+/', $str) == 1) return false;
+        return true;
     }
-    
+
+    public static function is_string_numeric($str)
+    {
+		if (!is_string($str)) return false;
+        if (preg_match('/[^0-9]+/', $str) == 1) return false;
+        return true;
+    }
+
     public static function filter(array $input, array $keys)
     {
         $out = [];
---
+++ input/Validator.test.php
@@ -0,0 +1,208 @@
+#!/usr/bin/php
+<?php
+require('Validator.php');
+
+fn_test(
+	'is_bool',
+	[
+        [true, true],
+        [false, true],
+        [0, false],
+        ['str', false],
+        [12.43, false],
+        [[], false],
+        [['-1'], false],
+    ]
+);
+
+fn_test(
+	'is_num',
+	[
+        [-9.3, true],
+        [-1, true],
+        [0, true],
+        [55, true],
+        [12.43, true],
+        ['2', true],
+        ['-1', true],
+        ['', false],
+        ['abc', false],
+        ['', false],
+    ]
+);
+
+fn_test(
+	'is_id_num',
+	[
+        [1, true],
+        [55, true],
+        [0, false],
+        [-1, false],
+        ['2', false],
+        ['-1', false],
+        ['', false],
+    ]
+);
+
+fn_test(
+	'is_id_num_array',
+	[
+        [[1,8,12], true],
+        [[], true],
+        [[-2,0,4.21], false],
+        [[1,8,76.9], false],
+        [[3,'a'], false],
+        [['', 43], false],
+    ]
+);
+
+fn_test(
+	'is_time',
+	[
+        ['10:45:12', true],
+        ['10:45:12.323', true],
+        ['1:5:2', true],
+        ['10:45', true],
+        ['-10:45', false],
+        ['10 45.34', false],
+        ['', false],
+        ['q:3:r', false],
+    ]
+);
+
+fn_test(
+	'is_date',
+	[
+        ['2019-2-1', true],
+        ['2019-21-17', true],
+        ['11-3-2', false],
+        ['2020 3 19', false],
+        ['', false],
+        ['q-3-r', false],
+    ]
+);
+
+fn_test(
+	'is_timestamp',
+	[
+        ['2019-2-17T23:22:1.3411', true],
+        ['2019-2-17 3:2:1.4545', true],
+        ['2019-2-17 23:22:1', true],
+        ['2019-2-17Y23:22:1', false],
+        ['209-2-17T23:22:12.1', false],
+        ['2019-2-17 23:12', false],
+    ]
+);
+
+fn_test(
+	'is_timestamp_range',
+	[
+        [['2019-2-17T0:0:0', '2019-2-17 23:22:1.3411'], true],
+        [['', '2019-2-17 23:22:1.3411'], true],
+        [[null, '2019-2-17 23:22:1.3411'], true],
+        [['2019-2-17T0:0:0', ''], true],
+        [['2019-2-17T0:0:0', null], true],
+        [['', ''], true],
+        [[null, null], true],
+        [['2019-2-17T0:0:0'], false],
+    ]
+);
+
+fn_test(
+	'is_not_empty_string',
+	[
+        ['jaz nisem prazen string', true],
+        ['', false],
+        [234, false],
+        [[2,5,'test',90], false],
+    ]
+);
+
+fn_test(
+	'is_string',
+	[
+        ['jaz sem string', true],
+        ['', true],
+        [234, false],
+        [[2,5,'test',90], false],
+    ]
+);
+
+fn_test(
+	'is_string_alphanumeric',
+	[
+        ['', true],
+        ['alfanum', true],
+        ['234', true],
+        ['alfanum234', true],
+        //['čšžćđČŠŽĆĐ', true],
+        ['alfanum 234', false],
+        ['(43 str).', false],
+        ['2*3+4=3 rezultat', false],
+        [234, false],
+        [['2',5,'test',90], false],
+    ]
+);
+
+fn_test(
+	'is_string_alpha',
+	[
+        ['', true],
+        ['alfanum', true],
+        ['234', false],
+        ['alfanum234', false],
+        //['čšžćđČŠŽĆĐ', true],
+        ['alfanum 234', false],
+        ['(43 str).', false],
+        ['2*3+4=3 rezultat', false],
+        [234, false],
+        [['2',5,'test',90], false],
+    ]
+);
+
+fn_test(
+	'is_string_numeric',
+	[
+        ['', true],
+        ['alfanum', false],
+        ['234', true],
+        ['alfanum234', false],
+        //['čšžćđČŠŽĆĐ', true],
+        ['alfanum 234', false],
+        ['(43 str).', false],
+        ['2*3+4=3 rezultat', false],
+        [234, false],
+        [['2',5,'test',90], false],
+    ]
+);
+
+
+
+fn_test(
+	'is_ip_address',
+	[
+        ['192.0.32.0', true],
+        ['1.2.0', false],
+        ['1.2.0.1234', false],
+        ['1.2 89', false],
+        ['127.2.er.4', false],
+        ['', false],
+    ]
+);
+
+###########################################
+function fn_test($fn_name, $test_cases)
+{
+	echo "Function: $fn_name\n";
+	$ret = null;
+	foreach ($test_cases as $case) {
+		$ret = Validator::$fn_name($case[0]);
+		if ($ret === $case[1]) {}//echo "OK\n";
+		else {
+			echo "FAIL: ";
+            print_r($case[0]);
+			echo "\nRETURNED ";
+			var_dump($ret);
+		}
+	}
+}
